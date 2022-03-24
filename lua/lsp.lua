6e4e00c17524e81fffa332ae48c713c983e6b83b
James van de Ven <hello@jamesvandeven.com>
2022-03-24 19:33:57 +0800

    Added C++ formatting

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,6 @@
 local lspconfig = require("lspconfig")
 local servers = {
+    "bashls",
 	"ccls",
 	"gopls",
 	"pyright",
@@ -27,6 +28,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.clang_format.with({
+			extra_args = { "--style", "{IndentWidth: 4}" },
+		}),
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.formatting.stylua,
